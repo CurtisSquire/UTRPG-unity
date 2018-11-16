4c59ed18e94535beb7c2b46f405e0019d3f00fe0
CurtisSquire <45086802+CurtisSquire@users.noreply.github.com>
2018-11-15 22:22:30 -0500

    Add files via upload

---
+++ Battle.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Battle : MonoBehaviour
+{
+    public float Speed;
+    public Transform target;
+    private float timer;
+    private float turntimer = 10;
+    public static bool NextTurn = false; 
+    private bool isMoveing;
+    public float moveDuration = 3f;
+    public float stayDuration = 2f;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        timer = 0; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MoveToPlayer();
+        UpdateTimer(); 
+    }
+
+    void MoveToPlayer()
+    {
+        if (isMoveing == true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+        }
+        else if (turntimer >= 10)
+        {
+            NextTurn = true; 
+        }
+
+    }
+    void UpdateTimer()
+    {
+        timer += Time.deltaTime;
+        turntimer += Time.deltaTime; 
+        if (isMoveing == true)
+        {
+            if (timer > moveDuration)
+            {
+                timer = 0; 
+                isMoveing = false;
+            }
+        }
+
+        if (isMoveing == false)
+        {
+            if (timer > stayDuration)
+            {
+                timer = 0; 
+                isMoveing = true;
+            }
+            
+            
+        } 
+    }
+}
+        
+   // }
+
+
+
---
+++ Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+    private float timer;
+    private bool isInvinsible;
+    public float TimerForInvincibility = 3f;
+    void Start()
+    {
+        timer = 0;     
+    }
+
+    void Update()
+    {
+        UpdateInvincibility();
+        
+    }
+
+    void UpdateInvincibility()
+    {
+        
+        if (isInvinsible == true)
+        {
+            timer += Time.deltaTime;
+            if (timer > TimerForInvincibility)
+            {
+                timer = 0;
+                isInvinsible = false;
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.CompareTag("Enemy"))
+        {
+            if (isInvinsible == false && Health.life > 0)
+            {
+                Destroy(c.gameObject);
+                Health.life -= 1;
+                isInvinsible = true; 
+                Debug.Log(Health.life);
+                Debug.Log(c.gameObject.name); 
+            }
+        }
+    }
+}
+
